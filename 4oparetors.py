21034a72a8de1a583453acad4ffae9dbae8d86e0
saikumarrella <saikumarrella03@gmail.com>
2025-10-15 22:49:15 +0530

    kumar

---
+++ 4oparetors.py
@@ -0,0 +1,37 @@
+"""arithemtic opretoers"""
+# a=int(input("enter the  first nummber"))
+# b=int(input("enter the second nummber"))
+# def add():
+#     return a+b
+# a=add()
+# print(a)#a=15
+# mul=a*b
+# sub=a-b
+# div=a/b
+# mod=a%b
+# foloor=a//b
+# power=a**2
+
+# print(mul,sub,div,mod,foloor,power)
+"""logical"""
+# a=int(input(" enter the number"))
+# b=int(input("enter the number"))
+# c=int(input("enter the nummber"))
+# if a>b and b>c:
+#     print("a is big")
+# elif b>a and b>c:
+#     print(" b is the big ")
+# else:
+#     print("c is the big")
+# if a==b or b==c or a==c:
+#     print("two number are the equal")
+# else:
+    
+#  print("no two number are not equal")
+"""identy oparetors"""
+a=12
+b=a
+c=48
+
+print(True if a is c else False)
+print(True if a is not c else False)
